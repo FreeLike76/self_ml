c15b596e82627e71809d398816aba58a8bbf1594
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2020-12-25 12:37:46 +0200

    Initial commit with a simple Logistic Regression model from SoloLearn

---
+++ p1_logistic_regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+#df = pd.read_csv("resources/titanic.csv")
+df = pd.read_csv("https://sololearn.com/uploads/files/titanic.csv")
+
+X = df[["Age", "Fare"]].values
+Y = df["Survived"].values
+
+model = LogisticRegression()
+model.fit(X, Y)
+
+lineX = np.linspace(0, 100, 100)
+lineY = -(model.coef_[0, 0]*lineX + model.intercept_)/model.coef_[0, 1]
+
+plt.scatter(df["Age"], df["Fare"], c=df["Survived"])
+plt.plot(lineX, lineY, c="red")
+
+plt.title("0 = (" + str(model.coef_[0, 0]) + ")*x + (" + str(model.coef_[0, 1]) + ")*y + (" + str(model.intercept_) + ");")
+
+plt.xlabel("Age")
+plt.ylabel("Fare")
+
+plt.grid()
+plt.show()
+
